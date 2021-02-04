48137d0a7e316390036011526621e156fd9ec485
SergeyVishnevskiy <serrzik13manager@gmail.com>
2021-02-04 08:31:42 +0200

    add

---
+++ src/components/FilterContacts/FilterContacts.js
@@ -0,0 +1,24 @@
+import React from "react";
+import styles from "./FilterConatcts.module.css";
+import { useDispatch } from "react-redux";
+import { filterEdit } from "../../redux/actions/filterAction";
+
+const FilterContacts = () => {
+  const dispatch = useDispatch();
+  const editFilter = ({ target }) => {
+    dispatch(filterEdit(target.value.toLowerCase()));
+  };
+  return (
+    <div className={styles.wrapper}>
+      <p className={styles.inputName}>Find contacts by name</p>
+      <input
+        type="text"
+        placeholder="Enter name"
+        onChange={editFilter}
+        className={styles.filter}
+      ></input>
+    </div>
+  );
+};
+
+export default FilterContacts;
