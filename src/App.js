087f361bb0dc5d5a23b45bf3a1e68a417b8d33f8
SergeyVishnevskiy <serrzik13manager@gmail.com>
2021-02-04 08:31:24 +0200

    add

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState, useEffect } from "react";
+import AddContact from "./components/AddContact/AddContact";
+import AllContacts from "./components/AllContacts/AllContacts";
+import FilterContacts from "./components/FilterContacts/FilterContacts";
+import Alert from "./components/Alert/Alert";
+import { CSSTransition } from "react-transition-group";
+import { useSelector, useDispatch } from "react-redux";
+import { userAdd } from "./redux/actions/itemsAction";
 
 function App() {
+  const [showAlert, setShowAlert] = useState({ status: false, text: "" });
+
+  const contacts = useSelector((state) => state.items);
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (!localStorage.getItem("localContacts")) {
+      localStorage.setItem("localContacts", JSON.stringify([]));
+    }
+    const users = JSON.parse(localStorage.getItem("localContacts"));
+    users.map((user) => dispatch(userAdd(user)));
+  }, [dispatch]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <CSSTransition
+        in={true}
+        appear={true}
+        timeout={500}
+        classNames="titlePhonebook"
+      >
+        <h1 className="titlePhonebook">Phonebook</h1>
+      </CSSTransition>
+
+      <AddContact setShowAlert={setShowAlert} />
+      {contacts.length >= 2 && <FilterContacts />}
+
+      {contacts.length > 0 && <AllContacts />}
+
+      <CSSTransition
+        in={showAlert.status}
+        timeout={250}
+        classNames="alert"
+        mountOnEnter
+        unmountOnExit
+      >
+        <Alert showAlert={showAlert.text} />
+      </CSSTransition>
     </div>
   );
 }
