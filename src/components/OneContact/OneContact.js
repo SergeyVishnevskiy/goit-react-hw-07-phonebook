0b73d4513e7327fc846ed55a29c6b0b7ec07d637
SergeyVishnevskiy <serrzik13manager@gmail.com>
2021-02-06 20:45:45 +0200

    add

--- src/components/OneContact/OneContact.js
+++ src/components/OneContact/OneContact.js
@@ -1,21 +1,15 @@
 import React from "react";
 import styles from "./OneContact.module.css";
-import { useDispatch } from "react-redux";
-import { deleteUserOption } from "../../redux/operation/itemsOperation";
 
-const OneContact = ({ name, number, id }) => {
-  const dispatch = useDispatch();
-  const userDeleteHandle = (id) => {
-    dispatch(deleteUserOption(id));
-  };
-
+const OneContact = ({ contact: { id, name, number }, userDeleteHandler }) => {
   return (
     <li className={styles.listItem}>
       <p className={styles.contactName}>{name}</p>
       <p className={styles.contactNumber}>{number}</p>
       <button
         className={styles.deleteButton}
-        onClick={() => userDeleteHandle(id)}
+        data-id={id}
+        onClick={userDeleteHandler}
       >
         Delete
       </button>
