608af9e95c4e55d88050fb512ffc2c0580832878
SergeyVishnevskiy <serrzik13manager@gmail.com>
2021-02-06 20:24:23 +0200

    add

--- src/redux/actions/filterAction.js
+++
@@ -1,9 +0,0 @@
-import { EDIT_FILTER } from "../constants/constants";
-import { createAction } from "@reduxjs/toolkit";
-
-export const filterEdit = createAction(EDIT_FILTER);
-
-// export const filterEdit = (value) => ({
-//   type: EDIT_FILTER,
-//   payload: value,
-// });
