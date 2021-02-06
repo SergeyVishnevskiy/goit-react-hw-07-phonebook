5eb7e87523f7525240f655ab1c240f721334e3a0
SergeyVishnevskiy <serrzik13manager@gmail.com>
2021-02-06 20:25:01 +0200

    add

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,11 @@
-import { createStore, applyMiddleware } from "redux";
-import { composeWithDevTools } from "redux-devtools-extension";
-import rootReducer from "./reducers/mainReducers";
-import thunk from "redux-thunk";
+import { configureStore } from "@reduxjs/toolkit";
+import rootReducer from "./reducers/rootReducer";
 
-const store = createStore(
-  rootReducer,
-  composeWithDevTools(applyMiddleware(thunk))
-);
-
+const store = configureStore({
+  reducer: rootReducer,
+});
 export default store;
+
+// import { composeWithDevTools } from "redux-devtools-extension";
+// const store = createStore(rootReducer, composeWithDevTools());
+//export default store
